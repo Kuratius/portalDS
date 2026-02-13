8b9f3c2a65fd9d1c4c9b012b685655dfa9079e0a
Kuratius <Kuratius@gmx.net>
2026-02-13 01:09:34 +0100

    math.c cleanup

--- arm7/source/math.c
+++ arm7/source/math.c
@@ -1,42 +1,22 @@
 #include "stdafx.h"
 
-
-#if 0
-#include "LUTs.h"
-ARM_CODE uint32_t sqrtv(uint32_t x)
-{
-	const uint16_t a=f32toint(x);
-	const uint16_t b=x&4095;
-	return (a<SQRTRANGE)?((a)?mulf32(sqrtLUT1[a],sqrtLUT2[b/(2*a)]):(sqrtLUT3[b])):(0);
-}
-
-#else 
 uint32_t isqrt_asm(uint32_t);
 
-
 ARM_CODE uint32_t sqrtv(uint32_t x)
 {
-	if (x<((1u<<20)-1 ))
+    if (x<(1u<<20))
     {
         return isqrt_asm(x<<12);
     } 
     uint32_t r0=isqrt_asm(x)<<6;
-#if 1
 
     uint32_t x0=((int64_t)r0*r0) >>12;
     uint32_t epsilon=x-x0;
-    
-   
     r0= r0+(epsilon<<11)/r0;
-#endif
+
     return r0;
 }
-#endif
 
-
-#if 1
-
-#if 1
 void normalize_arm7(int32_t * a);
 
 ARM_CODE vect3D normalize(vect3D v)
@@ -48,125 +28,60 @@ ARM_CODE vect3D normalize(vect3D v)
     v.z=a[2];
     return v;
 }
-#endif
-
-#else
-ARM_CODE __attribute__((always_inline)) static inline int32_t udiv16(int32_t n, uint32_t d){
-
-    int c=0;
-    if (n<0){
-        c=~c;
-        n=-n;
-    }
-    int N=16;
-    int l;    
-    if (__builtin_expect(d>1,1) ){
-        l=32-__builtin_clz(d-1);  //should be ceil(log2(d)). Isnt exactly that but difference isnt important;
-    } else 
-    {
-        l=0;
-    }    
-    uint32_t hi=((1ul<<l)-d)<<N ;
-    uint32_t mprime=hi/d;
-    mprime+=1;
-    int sh1= l>=1 ? 1: l;
-    int sh2= l-1 >=0 ? l-1 :0;
-    uint32_t t1=((uint64_t)mprime*(uint32_t)n)>>N;
-    uint32_t q= (t1+(((uint32_t)n-t1)>>sh1))>>sh2;
-    if (c)q=-q;    
-    return q;
-}
-
-ARM_CODE vect3D normalize(vect3D v)
-{
-	int32 d=sqrtv(((int64_t)v.x*v.x+(int64_t)v.y*v.y+(int64_t)v.z*v.z)>>12);
-    if (d<(1<<24)){
-        v.x<<=12;
-        v.y<<=12;
-        v.z<<=12;
-        
-    }else{
-        d>>=12;
-    }
-	return vect(udiv16(v.x,d),udiv16(v.y,d),udiv16(v.z,d));
-    
-}
-
-
-#endif
-
 
 void transposeMatrix33(int32_t *restrict m1, int32_t *restrict m2) //3x3
 {
-	int i, j;
-	for(i=0;i<3;i++)
-    {    
-        for(j=0;j<3;j++)
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++)
             m2[j+i*3]=m1[i+j*3];
-    }
     return;
 }
 
-
-
 ARM_CODE vect3D evalVectMatrix33(int32_t* m, vect3D v) //3x3
 {
-	return vect(((int64_t)v.x*m[0]+(int64_t)v.y*m[1]+(int64_t)v.z*m[2])>>12,
-				((int64_t)v.x*m[3]+(int64_t)v.y*m[4]+(int64_t)v.z*m[5])>>12,
-				((int64_t)v.x*m[6]+(int64_t)v.y*m[7]+(int64_t)v.z*m[8])>>12);
+    return vect(((int64_t)v.x*m[0]+(int64_t)v.y*m[1]+(int64_t)v.z*m[2])>>12,
+                ((int64_t)v.x*m[3]+(int64_t)v.y*m[4]+(int64_t)v.z*m[5])>>12,
+                ((int64_t)v.x*m[6]+(int64_t)v.y*m[7]+(int64_t)v.z*m[8])>>12);
 }
-
 
 ARM_CODE void multMatrix33(int32* m1, int32* m2, int32 *restrict m) //3x3
 {
-	int i, j;
-	// for(i=0;i<4;i++)for(j=0;j<4;j++)m[i+j*4]=m1[i+0*4]*m2[0+j*4]+m1[i+1*4]*m2[1+j*4]+m1[i+2*4]*m2[2+j*4]+m1[i+3*4]*m2[3+j*4];
-	for(i=0;i<3;i++)for(j=0;j<3;j++)m[j+i*3]=((int64_t)m1[0+i*3]*m2[j+0*3]+(int64_t)m1[1+i*3]*m2[j+1*3]+(int64_t)m1[2+i*3]*m2[j+2*3])>>12;
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++)
+            m[j+i*3]=((int64_t)m1[0+i*3]*m2[j+0*3]
+                     +(int64_t)m1[1+i*3]*m2[j+1*3]
+                     +(int64_t)m1[2+i*3]*m2[j+2*3])>>12;
 }
 
 ARM_CODE void multMatrix332(int32* m1, int32* m2, int32 *restrict m) //3x3
 {
-	int i, j;
-	for(i=0;i<3;i++)for(j=0;j<3;j++)m[i+j*3]=((int64_t)m1[i+0*3]*m2[0+j*3]+(int64_t)m1[i+2*3]*m2[2+j*3]+(int64_t)m1[i+2*3]*m2[2+j*3])>>12;
+    for(int i=0;i<3;i++)
+        for(int j=0;j<3;j++)
+            m[i+j*3]=((int64_t)m1[i+0*3]*m2[0+j*3]
+                     +(int64_t)m1[i+2*3]*m2[2+j*3]
+                     +(int64_t)m1[i+2*3]*m2[2+j*3])>>12;
 }
 
 ARM_CODE void rotateMatrixAxis(int32* tm, int32 x, vect3D a, bool r)
 {
-	int32 rm[9], m[9];
-
-	int32 cosval=cosLerp(x);
-	int32 sinval=sinLerp(x);
-	int32 onemcosval=inttof32(1)-cosval;
-
-	rm[0]=cosval+mulf32(mulf32(a.x,a.x),onemcosval);
-#if 0
-	rm[1]=mulf32(mulf32(a.x,a.y),onemcosval)-mulf32(a.z,sinval);
-	rm[2]=mulf32(mulf32(a.x,a.z),onemcosval)+mulf32(a.y,sinval);
-
-	rm[3]=mulf32(mulf32(a.x,a.y),onemcosval)+mulf32(a.z,sinval);
-	rm[4]=cosval+mulf32(mulf32(a.y,a.y),onemcosval);
-	rm[5]=mulf32(mulf32(a.y,a.z),onemcosval)-mulf32(a.x,sinval);
-
-	rm[6]=mulf32(mulf32(a.x,a.z),onemcosval)-mulf32(a.y,sinval);
-	rm[7]=mulf32(mulf32(a.y,a.z),onemcosval)+mulf32(a.x,sinval);
-	rm[8]=cosval+mulf32(mulf32(a.z,a.z),onemcosval);
-
-#else
-	rm[1]=((int64_t)mulf32(a.x,a.y)*onemcosval+(int64_t)-a.z*sinval)>>12;
-	rm[2]=((int64_t)mulf32(a.x,a.z)*onemcosval+(int64_t)a.y*sinval)>>12;
-
-	rm[3]=((int64_t)mulf32(a.x,a.y)*onemcosval+(int64_t)a.z*sinval)>>12;
-	rm[4]=cosval+mulf32(mulf32(a.y,a.y),onemcosval);
-	rm[5]=((int64_t)mulf32(a.y,a.z)*onemcosval+(int64_t)-a.x*sinval)>>12;
-
-	rm[6]=((int64_t)mulf32(a.x,a.z)*onemcosval+(int64_t)-a.y*sinval)>>12;
-	rm[7]=((int64_t)mulf32(a.y,a.z)*onemcosval+(int64_t)a.x*sinval)>>12;
-	rm[8]=cosval+mulf32(mulf32(a.z,a.z),onemcosval);
-#endif
-
-	if(r)multMatrix33(rm,tm,m);
-	else multMatrix33(tm,rm,m);
-	memcpy(tm,m,9*sizeof(int32));
+    int32 rm[9], m[9];
+    int32 cosval=cosLerp(x);
+    int32 sinval=sinLerp(x);
+    int32 onemcosval=inttof32(1)-cosval;
+    rm[0]=cosval+mulf32(mulf32(a.x,a.x),onemcosval);
+    rm[1]=((int64_t)mulf32(a.x,a.y)*onemcosval+(int64_t)-a.z*sinval)>>12;
+    rm[2]=((int64_t)mulf32(a.x,a.z)*onemcosval+(int64_t)a.y*sinval)>>12;
+    rm[3]=((int64_t)mulf32(a.x,a.y)*onemcosval+(int64_t)a.z*sinval)>>12;
+    rm[4]=cosval+mulf32(mulf32(a.y,a.y),onemcosval);
+    rm[5]=((int64_t)mulf32(a.y,a.z)*onemcosval+(int64_t)-a.x*sinval)>>12;
+    rm[6]=((int64_t)mulf32(a.x,a.z)*onemcosval+(int64_t)-a.y*sinval)>>12;
+    rm[7]=((int64_t)mulf32(a.y,a.z)*onemcosval+(int64_t)a.x*sinval)>>12;
+    rm[8]=cosval+mulf32(mulf32(a.z,a.z),onemcosval);
+    if(r)
+        multMatrix33(rm,tm,m);
+    else
+        multMatrix33(tm,rm,m);
+    memcpy(tm,m,9*sizeof(int32));
 }
 
 ARM_CODE static inline void asm_crossf32(const int32_t *a,const int32_t * b,int32_t *result)
@@ -204,7 +119,6 @@ ARM_CODE static inline void asm_crossf32(const int32_t *a,const int32_t * b,int3
 
 ARM_CODE vect3D vectProduct(vect3D v1, vect3D v2)
 {
-	//return vect(((int64_t)v1.y*v2.z+(int64_t)v1.z*-v2.y)>>12,((int64_t)v1.z*v2.x+(int64_t)-v1.x*v2.z)>>12,((int64_t)-v1.x*-v2.y+(int64_t)-v1.y*v2.x)>>12);
     int32_t result[3];
     if (sizeof(vect3D)!=3*sizeof(int32_t))
     {
@@ -216,31 +130,23 @@ ARM_CODE vect3D vectProduct(vect3D v1, vect3D v2)
     {
         asm_crossf32((const int32_t*)&v1,(const int32_t*) &v2,&result[0]);     
     }
-
-    
     return vect(result[0],result[1], result[2]);
 }
 
-
-
-
-
 void fixMatrix(int32* m) //3x3
 {
-	if(!m)return;
-	vect3D x=vect(m[0],m[3],m[6]);
-	vect3D y=vect(m[1],m[4],m[7]);
-	vect3D z=vect(m[2],m[5],m[8]);
-	
-	projectVectorPlane(&x,y);
-	projectVectorPlane(&z,y);
-	projectVectorPlane(&z,x);
-	
-	x=normalize(x);
-	y=normalize(y);
-	z=normalize(z);
-	
-	m[0]=x.x;m[3]=x.y;m[6]=x.z;
-	m[1]=y.x;m[4]=y.y;m[7]=y.z;
-	m[2]=z.x;m[5]=z.y;m[8]=z.z;
+    if(!m)
+        return;
+    vect3D x=vect(m[0],m[3],m[6]);
+    vect3D y=vect(m[1],m[4],m[7]);
+    vect3D z=vect(m[2],m[5],m[8]);
+    projectVectorPlane(&x,y);
+    projectVectorPlane(&z,y);
+    projectVectorPlane(&z,x);
+    x=normalize(x);
+    y=normalize(y);
+    z=normalize(z);
+    m[0]=x.x;m[3]=x.y;m[6]=x.z;
+    m[1]=y.x;m[4]=y.y;m[7]=y.z;
+    m[2]=z.x;m[5]=z.y;m[8]=z.z;
 }
