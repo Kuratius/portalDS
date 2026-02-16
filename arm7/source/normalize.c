fbeca08592caaceb9ab76398a4ce23c362158925
Kuratius <kuratius@gmx.net>
2026-02-16 19:14:15 +0100

    add missing license

--- arm7/source/normalize.c
+++ arm7/source/normalize.c
@@ -1,3 +1,20 @@
+//Copyright (C) 2026 Dominik Kurz
+
+//This program is free software; you can redistribute it and/or
+//modify it under the terms of the GNU Lesser General Public
+//License as published by the Free Software Foundation; either
+//version 3 of the License, or (at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//Lesser General Public License for more details.
+
+//You should have received a copy of the GNU Lesser General Public License
+//along with this program; if not, write to the Free Software Foundation,
+//Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
 #include <nds.h>
 #include <stdint.h>
 ARM_CODE static uint32_t sqrt_core(uint32_t x, uint32_t y)
